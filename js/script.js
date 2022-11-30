6fdf5eba226119670305955fd1b41ce5cb23eddf
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-12-01 00:05:39 +0100

    inserito commenti e chiarito il codice

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const app = createApp({
         return {
             newTodo: '',
             todos:[],
+            apiUrl: 'server.php',
             alarm: false,
             colors :['#ff000080','#ff870080','#ffd30080','#a1ff0a80','#0aff9980','#0aefff80','#147df580','#580aff80','#be0aff80'], //colori per i todo
             colorsCircle :['#ff0000','#ff8700','#ffd300','#a1ff0a','#0aff99','#0aefff','#147df5','#580aff','#be0aff'] //colori per i pallini iniziali
@@ -12,7 +13,8 @@ const app = createApp({
     },
     methods:{
         getList(){
-            axios.get('server.php').then(
+            const url = this.apiUrl;
+            axios.get(url).then(
                 (response) => {
                     this.todos=[...response.data];
                 }
@@ -20,44 +22,47 @@ const app = createApp({
         },
         addTask(){
             if(this.newTodo.length >= 3){
+                const url = this.apiUrl + '?add';
                 const data = {newTodo : this.newTodo};
-                axios.post('server.php',
+                axios.post(url,
                 data,
                 {headers : {'Content-Type': 'multipart/form-data'}}
                 ).then(
                     (response) => {
-                        // console.log(response.data)
                         this.getList();
                     }
                 )
                 this.newTodo = '';
                 this.alarm = false;
             }else{
+                //variabile per far comparire il messaggio di errore
                 this.alarm = true;
                 setTimeout(()=> this.alarm=false,1500)
                 this.newTodo = '';
             }
         },
         removeTask(i){
+            const url = this.apiUrl + '?delete';
             const data = {index : i};
-            axios.post('server.php?delete',
+            //passo l'indice dell'elemento da togliere e nell'url specifico l'azione che faccio
+            axios.post(url,
             data,
             {headers : {'Content-Type': 'multipart/form-data'}},
             ).then(
                 (response) =>{
-                    // console.log(response.data);
                     this.getList();
                 }
             )
         },
         taskDone(i){
+            const url = this.apiUrl + '?done';
             const data = {index : i};
-            axios.post('server.php?done',
+            //passo l'indice dell'elemento da segnare fatto e nell'url specifico l'azione che faccio
+            axios.post(url,
             data,
             {headers : {'Content-Type': 'multipart/form-data'}},
             ).then(
                 (response) =>{
-                    // console.log(response.data);
                     this.getList();
                 }
             )
--- server.php
+++ server.php
@@ -1,26 +1,35 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-Requested-With");
-$textFile = file_get_contents('./data.json');
+$filePath = './data.json';
+$textFile = file_get_contents($filePath);
+//associo alla variabile todos l'array preso dal json
 $todos = json_decode($textFile);
 
-if(isset($_POST['newTodo'])){
+//controllo se è settato il nuovo elemento e se l'azione è di aggiunta
+if(isset($_POST['newTodo']) && isset($_GET['add'])){
+    //creo il nuovo oggetto lo pusho e sovrascrivo il file
     $newTodo = [
         'testo' => $_POST['newTodo'],
         'done' => false
     ];
     array_unshift($todos,$newTodo);
-    file_put_contents('./data.json', json_encode($todos));
+    file_put_contents($filePath, json_encode($todos));
+    //controllo se è settato l'indice e se l'azione è di segnare task completata
 } elseif(isset($_POST['index']) && isset($_GET['done'])){
+    //modifico il valore del done nell'oggetto corretto e sovrascrivo il file
     $i = intval($_POST['index']);
     $todos[$i]->done = !$todos[$i]->done;
-    file_put_contents('./data.json', json_encode($todos));
+    file_put_contents($filePath, json_encode($todos));
+    //controllo se è settato l'indice e se l'azione è di rimuovere la task
 }elseif(isset($_POST['index']) && isset($_GET['delete'])){
-    var_dump('sono qui');
+    //tolgo l'elemento corretto e sovrascrivo il file
     $i = intval($_POST['index']);
     array_splice($todos,$i,1);
-    file_put_contents('./data.json', json_encode($todos));
+    file_put_contents($filePath, json_encode($todos));
+
 }else{
+
     header('Content-Type: application/json');
     echo json_encode($todos);
 };
